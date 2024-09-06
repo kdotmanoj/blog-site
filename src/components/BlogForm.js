e5f4d04276447c7f4308c68621bd98b2f33457e9
kdotmanoj <hsuper681@gmail.com>
2024-09-06 18:40:16 +0530

    Made few enhancements

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,23 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { TextField, Button, Container, MenuItem, Select, InputLabel, FormControl } from '@mui/material';
-import { Editor } from '@tinymce/tinymce-react'; 
+import { Editor } from '@tinymce/tinymce-react';
 
-const BlogForm = () => {
+const BlogForm = ({ theme }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { id } = useParams();
   const blog = useSelector(state => state.blogs.blogs.find(blog => blog.id === id)) || {};
 
   const [title, setTitle] = useState(blog.title || '');
-  const [content, setContent] = useState(blog.content || '');
   const [author, setAuthor] = useState(blog.author || '');
   const [image, setImage] = useState(blog.image || '');
   const [category, setCategory] = useState(blog.category || '');
+  const [content, setContent] = useState('');
 
-  // List of categories
   const categories = ['Technology', 'Travel', 'Food', 'Lifestyle'];
+
+  useEffect(() => {
+    if (blog.content) {
+      setContent(blog.content);
+    }
+  }, [blog.content]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -76,12 +81,12 @@ const BlogForm = () => {
         </FormControl>
         <Editor
           apiKey={process.env.REACT_APP_TINYMCE_API_KEY}
-          initialValue={content}
+          value={content}
           init={{
             height: 500,
             menubar: false,
             plugins: [
-              'advlist autolink lists link image charmap preview anchor textcolor',
+              'advlist autolink lists link image charmap preview anchor',
               'searchreplace visualblocks code fullscreen',
               'insertdatetime media table paste code help wordcount'
             ],
@@ -89,9 +94,13 @@ const BlogForm = () => {
               'undo redo | formatselect | bold italic backcolor | \
               alignleft aligncenter alignright alignjustify | \
               bullist numlist outdent indent | removeformat | help',
-            content_css: 'https://www.tiny.cloud/css/codepen.min.css'
+            content_css: theme === 'dark' 
+              ? 'https://www.tiny.cloud/css/dark-theme.css' 
+              : 'https://www.tiny.cloud/css/light-theme.css',
+            skin: theme === 'dark' ? 'oxide-dark' : 'oxide',
+            placeholder: 'Write your blog here...',  
           }}
-          onChange={(e) => setContent(e.target.getContent())}
+          onEditorChange={(newContent) => setContent(newContent)}
         />
         <Button type="submit" variant="contained" color="primary" style={{ marginTop: '20px' }}>
           {id ? 'Update Blog' : 'Create Blog'}
